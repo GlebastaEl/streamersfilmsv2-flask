e849c07371f023361cf8616e2cf6b2fd5250601f
GlebastaEl <gleboden19023005@gmail.com>
2025-05-28 02:18:40 +0300

    scripts

--- static/neonBlickScript.js
+++ static/neonBlickScript.js
@@ -1,29 +1,28 @@
-const neons = document.querySelectorAll('.addAtmosphere');
-const sound = document.getElementById('flicker-sound');
-let interval;
+// Проверка, является ли устройство мобильным
+const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 
-for (let neon of neons) {
+if (!isMobile) {
+    const neons = document.querySelectorAll('.addAtmosphere');
+    const sound = document.getElementById('flicker-sound');
+    let interval;
 
-neon.addEventListener('mouseenter', () => {
-    // Включаем звук
-    sound.currentTime = 0;
-    sound.play();
+    for (let neon of neons) {
+        neon.addEventListener('mouseenter', () => {
+            sound.currentTime = 0;
+            sound.play();
 
-    interval = setInterval(() => {
-        // 30% шанс мерцания
-        if (Math.random() > 0.7) {
-            neon.classList.toggle('flicker-off');
-        }
-    }, 100);
-});
+            interval = setInterval(() => {
+                if (Math.random() > 0.7) {
+                    neon.classList.toggle('flicker-off');
+                }
+            }, 100);
+        });
 
-neon.addEventListener('mouseleave', () => {
-    clearInterval(interval);
-    neon.classList.remove('flicker-off');
-
-    // Останавливаем звук и сбрасываем его
-    sound.pause();
-    sound.currentTime = 0;
-});
-
+        neon.addEventListener('mouseleave', () => {
+            clearInterval(interval);
+            neon.classList.remove('flicker-off');
+            sound.pause();
+            sound.currentTime = 0;
+        });
+    }
 }
